42dc2037e5a174927096bab7670b64d8395c26ed
lpotte <lpotte@uninorte.edu.co>
2019-11-13 16:11:43 -0500

    prueba funcinal

--- php/configuration.php
+++ php/configuration.php
@@ -1,7 +1,12 @@
 <?php
-$db_host = 'webservice_db_1';
-$db_username = 'root';
-$db_password = 'password';
-$db_schema = 'test_db';
-date_default_timezone_set("America/Bogota");
+    $db_host = 'webservice_db_1';
+    $db_username = 'root';
+    $db_password = 'password';
+    $db_schema = 'universidad';
+    date_default_timezone_set("America/Bogota");
+
+    $test = ['hola'];
+    header('Content-Type: application/json');
+    echo json_encode($test);
+
 ?>
