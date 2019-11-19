46a5c0129d8851522c9ad9875e5126f808a62416
Diego Albor Rojas <daalbor@uninorte.edu.co>
2019-11-19 18:11:04 -0500

    Personas agregadas

---
+++ BD/AddData.sql
@@ -0,0 +1,72 @@
+use COLUNCOL;
+/*Persona*/
+INSERT INTO Persona VALUES (72245873,'Pedro','Antonio','Albor','Reyes','M');
+INSERT INTO Persona VALUES (1001851557,'Clara','Lucia','Parra','Nieto','F');
+INSERT INTO Persona VALUES (8701199,'Roberto','Alfonso','Parra','Silguero','M');
+INSERT INTO Persona VALUES (32758353,'Ledys','Bird','Nieto','Berrio','F');
+INSERT INTO Persona VALUES (22420082,'Margarita',NULL,'Parra','Silguero','F');
+/*Sin Segundo Nombre*/
+INSERT INTO Persona VALUES(1193412215,'Isabella',NULL,'Vengoechea','Visbal','F');
+INSERT INTO Persona VALUES(87921020,'Paola',NULL,'Gutierrez','Vesga','F');
+INSERT INTO Persona VALUES(1001810022,'Isabella',NULL,'Vengoechea','Visbal','F');
+INSERT INTO Persona VALUES(1001111029,'Ana',NULL,'Lozano','Caballero','F');
+INSERT INTO Persona VALUES(1001123090,'Sixto',NULL,'Gomez','Sarmiento','M');
+INSERT INTO Persona VALUES(87019823,'Daniel',NULL,'Hernandez','Gomez','M');
+INSERT INTO Persona VALUES(32758654,'Veronica',NULL,'Estrada','Ortega','F');
+INSERT INTO Persona VALUES(32876987,'Natalia',NULL,'Cardenas','Carrillo','F');
+INSERT INTO Persona VALUES(1001782929,'Gabriela',NULL,'Otero','Verdooren','F');
+INSERT INTO Persona VALUES(1001565787,'Keissy',NULL,'Cerra','Hernandez','F');
+INSERT INTO Persona VALUES(1001898676,'Hillary',NULL,'Gomez','Mejia','F');
+INSERT INTO Persona VALUES(1001232454,'Maria',NULL,'Otero','Quintero','F');
+INSERT INTO Persona VALUES(1001251251,'Lilibeth',NULL,'Ruiz','Ponce','F');
+INSERT INTO Persona VALUES(1001802490,'Eliana',NULL,'Parra','Sanchez','F');
+INSERT INTO Persona VALUES(1001557554,'Natalia',NULL,'Guevara','Potes','F');
+INSERT INTO Persona VALUES(1001009570,'Elvia',NULL,'Morelo','Morales','F');
+INSERT INTO Persona VALUES(2278173639,'Nigel',NULL,'Davila','Acuña','M');
+INSERT INTO Persona VALUES(1082525609,'Kevin',NULL,'Ruz','Nuñez','M');
+INSERT INTO Persona VALUES(1002038900,'Gabriel',NULL,'Gomez','Suarez','M');
+INSERT INTO Persona VALUES(1001092012,'Valentina',NULL,'Tatis','Solano','F');
+/*Con segundo nombre*/
+INSERT INTO Persona VALUES (1001043371,'Glennys','Margaraita','Parra','Nieto','F');
+INSERT INTO Persona VALUES (1004098737,'Kendra','Johana','Daza','Guerra','F');
+INSERT INTO Persona VALUES (1005093878,'Daniela','Andrea','Diaz','Marriaga','F');
+INSERT INTO Persona VALUES (1001834923,'Daniela','Paola','Maestre','Iglesias','F');
+INSERT INTO Persona VALUES (1001372935,'Maria','Alejandra','Guarín','Robles','F');
+INSERT INTO Persona VALUES (1001928333,'Luz','Celeste','Payares','Simanca','F');
+INSERT INTO Persona VALUES (1005920001,'Kiara','Estefany','Figueroa','Carbono','F');
+INSERT INTO Persona VALUES (1004040005,'Angie','Paola','Becerra','Bohorquez','F');
+INSERT INTO Persona VALUES (1001999333,'Juan','Esteban','Biswell','Jimenez','M');
+INSERT INTO Persona VALUES (1001283923,'Luz','Karime','Rabelo','Bohorquez','F');
+INSERT INTO Persona VALUES (1001939939,'Maria','Elvira','Duarte','Conde','F');
+INSERT INTO Persona VALUES (1001222222,'Jhon','David','Restrepo','Berrio','M');
+INSERT INTO Persona VALUES (1001919829,'Angie','Paola','Trujillo','Payares','F');
+INSERT INTO Persona VALUES (1001101001,'Carlos','Arturo','Obregon','Meza','M');
+INSERT INTO Persona VALUES (1001283457,'Andrea','Camila','Pertuz','Molina','F');
+INSERT INTO Persona VALUES (1001098123,'Camila','Mar','Valencia','Santiago','F');
+INSERT INTO Persona VALUES (1001898227,'Diana','Marcela','Torres','Landazury','F');
+INSERT INTO Persona VALUES (1001792787,'Maria','Valentina','Herrera','Rivera','F');
+INSERT INTO Persona VALUES (1001828919,'Christian','Camilo','Forero','Iriarte','M');
+INSERT INTO Persona VALUES (1001335885,'Laura','Valentina','Guerrero','Negrete','F');
+INSERT INTO Persona VALUES (1001838834,'Shaleen','Maria','Dennis','Velez','F');
+INSERT INTO Persona VALUES (1001123874,'Leandro','Enrique','Puello','Barrios','M');
+INSERT INTO Persona VALUES (1001343535,'Karen','Paola','Herrera','Rivera','F');
+INSERT INTO Persona VALUES (1001990120,'Maria','Carolina','Quintero','Carmona','F');
+INSERT INTO Persona VALUES (1001767401,'Nayibis','Danelly','Almanza','Ayos','F');
+INSERT INTO Persona VALUES (1001092803,'Angie','Victoria','Zuñiga','Figueroa','F');
+INSERT INTO Persona VALUES (1001302143,'Angela','Patricia','Camargo','Ramos','F');
+INSERT INTO Persona VALUES (1047347780,'Aldair','Jose','Zapata','De La Hoz','M');
+INSERT INTO Persona VALUES (1002909029,'Eyleen','Paola','Moreno','Gutierrez','F');
+INSERT INTO Persona VALUES (1001938920,'Erick','Alberto','Solano','De La Hoz','M');
+INSERT INTO Persona VALUES (1002203945,'Jorge','Andres','Molinares','Perez','M');
+INSERT INTO Persona VALUES (1002839403,'Glenda','Alejandra','Gutierrez','Vizcaino','F');
+INSERT INTO Persona VALUES (1002892018,'Angelica','Maria','Gomez','Suarez','F');
+INSERT INTO Persona VALUES (1278293848,'Darlis','Carolina','Pineda','Meza','F');
+INSERT INTO Persona VALUES (1002879403,'Laura','Isabel','Morales','Ojeda','F');
+INSERT INTO Persona VALUES (1001923432,'Maria','Fernanda','Martinez','Mercado','F');
+INSERT INTO Persona VALUES (1001565756,'Sharay','Isabel','Martinez','Ojeda','F');
+INSERT INTO Persona VALUES (1001029302,'Eneida','Maria','Martinez','Ojeda','F');
+
+
+SELECT *
+FROM Persona
+ORDER BY nombre1
