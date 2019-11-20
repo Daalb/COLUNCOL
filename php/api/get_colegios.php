d68afbb70718af18fe75253573813eb2c0328aca
lpotte <lpotte@uninorte.edu.co>
2019-11-19 21:44:11 -0500

    apis update

---
+++ php/api/get_colegios.php
@@ -0,0 +1,29 @@
+<?php
+  include('configuration.php');
+  header('Content-Type: application/json');
+
+  $query = "SELECT * FROM Colegio";
+  $result = mysqli_query($connection,$query);
+
+  $colegios = array();
+
+  while($row = mysqli_fetch_array($result)) 
+  { 
+      $id=$row['id_colegio'];
+      $Nombre=$row['nombre'];
+      $sg=$row['siglas'];
+      $web=$row['pag_web'];
+      $correo=$row['correo'];
+      $registro=$row['id_registro'];
+
+      $colegios[] = array('id_colegio'=> $id, 'nombre'=> $nombre, 'siglas'=> $sg, 
+                       'pag_web'=> $web, 'correo'=> $correo, 'id_registro'=> $registro);
+
+  }
+
+  $close = mysqli_close($conexion) 
+  or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+    
+  $json_string = json_encode($colegios);
+  echo $json_string;
+ ?>
