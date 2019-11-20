8d20b9e5d49b6240dd6cb7bf228257dcc4057861
lpotte <lpotte@uninorte.edu.co>
2019-11-20 09:26:30 -0500

    bugs fixed

--- php/api/get_personas.php
+++ php/api/get_personas.php
@@ -8,9 +8,14 @@ $array = array();
 
 while ($row = mysqli_fetch_array($result)) {
     $id = $row['id_persona'];
-    $nombre = $row['nombre1'];
+    $nombre1 = $row['nombre1'];
+    $nombre2 = $row['nombre2'];
+    $apellido1 = $row['apellido1'];
+    $apellido2 = $row['apellido2'];
+    $sexo = $row['sexo'];
 
-    $array[] = array('id_persona' => $id, 'nombre1' => $nombre);
+    $array[] = array('id_persona' => $id, 'nombre1' => $nombre1, 'nombre2' => $nombre2,
+                    'apellido1' => $apellido1,'apellido2' => $apellido2, 'sexo' => $sexo);
 }
 
 $close = mysqli_close($connection)
