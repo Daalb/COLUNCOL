6d77f49e87e5cbcb63317059b3d4e628801752f4
lpotte <lpotte@uninorte.edu.co>
2019-11-19 22:05:22 -0500

    apis update

--- php/api/get_asignaturas.php
+++ php/api/get_asignaturas.php
@@ -21,7 +21,7 @@
   $close = mysqli_close($connection) 
   or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
     
-  $json_string = json_encode($asignauras);
+  $json_string = json_encode($asignaturas);
   echo $json_string;
 
  ?>
