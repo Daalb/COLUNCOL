2e60d917b4a934893053518ae62feb3f779fca1a
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2019-11-19 23:09:21 -0500

    some changes

--- php/api/get_colegios.php
+++ php/api/get_colegios.php
@@ -1,29 +1,25 @@
 <?php
-  include('configuration.php');
-  header('Content-Type: application/json');
+include('configuration.php');
+header('Content-Type: application/json');
 
-  $query = "SELECT * FROM Colegio";
-  $result = mysqli_query($connection,$query);
+$query = "SELECT * FROM Colegio";
+$result = mysqli_query($connection, $query);
 
-  $colegios = array();
+$colegios = array();
 
-  while($row = mysqli_fetch_array($result)) 
-  { 
-      $id=$row['id_colegio'];
-      $Nombre=$row['nombre'];
-      $sg=$row['siglas'];
-      $web=$row['pag_web'];
-      $correo=$row['correo'];
-      $registro=$row['id_registro'];
+while ($row = mysqli_fetch_array($result)) {
+    $id = $row['id_colegio'];
+    $Nombre = $row['nombre'];
+    $sg = $row['siglas'];
+    $web = $row['pag_web'];
+    $correo = $row['correo'];
+    $registro = $row['id_registro'];
 
-      $colegios[] = array('id_colegio'=> $id, 'nombre'=> $nombre, 'siglas'=> $sg, 
-                       'pag_web'=> $web, 'correo'=> $correo, 'id_registro'=> $registro);
+    $colegios[] = array('id_colegio' => $id, 'nombre' => $Nombre, 'siglas' => $sg, 'pag_web' => $web, 'correo' => $correo, 'id_registro' => $registro);
+}
 
-  }
+$close = mysqli_close($connection)
+or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
 
-  $close = mysqli_close($connection) 
-  or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
-    
-  $json_string = json_encode($colegios);
-  echo $json_string;
- ?>
+$json_string = json_encode($colegios);
+echo $json_string;
--- php/api/get_personas.php
+++ php/api/get_personas.php
@@ -1,24 +1,21 @@
 <?php
-  include('configuration.php');
-  header('Content-Type: application/json');
+include('configuration.php');
+header('Content-Type: application/json');
 
-  $query = "SELECT * FROM Persona";
-  $result = mysqli_query($connection,$query);
+$query = "SELECT * FROM Persona";
+$result = mysqli_query($connection, $query);
 
-  $array = array();
+$array = array();
 
-  while($row = mysqli_fetch_array($result)) 
-  { 
-      $id=$row['id_persona'];
-      $nombre=$row['nombre1'];
+while ($row = mysqli_fetch_array($result)) {
+    $id = $row['id_persona'];
+    $nombre = $row['nombre1'];
 
-      $array[] = array('id_persona'=> $id, 'nombre1'=> $nombre);
+    $array[] = array('id_persona' => $id, 'nombre1' => $nombre);
+}
 
-  }
+$close = mysqli_close($connection)
+or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
 
-  $close = mysqli_close($connection) 
-  or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
-    
-  $json_string = json_encode($array);
-  echo $json_string;
- ?>
+$json_string = json_encode($array);
+echo $json_string;
