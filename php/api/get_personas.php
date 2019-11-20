c9c1a0f4767f70051d777db59092347a85e379fa
lpotte <lpotte@uninorte.edu.co>
2019-11-19 22:13:15 -0500

    apis update

---
+++ php/api/get_personas.php
@@ -0,0 +1,24 @@
+<?php
+  include('configuration.php');
+  header('Content-Type: application/json');
+
+  $query = "SELECT * FROM Persona";
+  $result = mysqli_query($connection,$query);
+
+  $array = array();
+
+  while($row = mysqli_fetch_array($result)) 
+  { 
+      $id=$row['id_persona'];
+      $nombre=$row['nombre1'];
+
+      $array[] = array('id_persona'=> $id, 'nombre1'=> $nombre);
+
+  }
+
+  $close = mysqli_close($connection) 
+  or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+    
+  $json_string = json_encode($array);
+  echo $json_string;
+ ?>
