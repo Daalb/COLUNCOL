6a71a5d4756bf5c946de0cc9d1625f4059a7de21
lpotte <lpotte@uninorte.edu.co>
2019-11-20 09:13:41 -0500

    bugs fixed

--- php/api/get_colegios.php
+++ php/api/get_colegios.php
@@ -1,25 +1,24 @@
 <?php
-include('configuration.php');
+    include('configuration.php');
 
-$query = "SELECT * FROM Colegio";
-$result = mysqli_query($connection, $query);
+    $query = "SELECT nombre FROM Colegio";
+    $result = mysqli_query($connection, $query);
 
-$colegios = array();
+    $colegios = array();
 
-while ($row = mysqli_fetch_array($result)) {
-    $id = $row['id_colegio'];
-    $Nombre = $row['nombre'];
-    $sg = $row['siglas'];
-    $web = $row['pag_web'];
-    $correo = $row['correo'];
-    $registro = $row['id_registro'];
+    while ($row = mysqli_fetch_array($result)) {
+        $id = $row['id_colegio'];
+        //$nom = $row['nombre'];
+        $sg = $row['siglas'];
+        $web = $row['pag_web'];
+        $correo = $row['correo'];
 
-    $colegios[] = array('id_colegio' => $id, 'nombre' => $Nombre, 'siglas' => $sg, 'pag_web' => $web, 'correo' => $correo, 'id_registro' => $registro);
-}
+        $colegios[] = array('id_colegio' => $id, /*'nombre' => $nom,*/ 'siglas' => $sg, 'pag_web' => 
+        $web, 'correo' => $correo);
+    }
 
-$close = mysqli_close($connection)
-or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
-
-$json_string = json_encode($colegios);
-echo $json_string;
+    $close = mysqli_close($connection)
+    or die("Ha sucedido un error inexperado en la desconexion de la base de datos");
+    $json_string = json_encode($colegios);
+    echo $json_string;
 ?>
